f5b6e97760cc22101fea76c2fd869ba0c4906ce9
PUBGPlayer3 <159410511+PUBGPlayer3@users.noreply.github.com>
2024-02-10 19:00:58 +0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,22 @@
+
+# step 1
+step_1 = goto("link mentioned in readme file https://vimeo.com/911518220")
+
+# step 2
+step_2 = watch("video on the vimeo")
+
+# step 3
+step_3 = find_link("find the discord link (discord.gg/5MS29Mxczw) mentioned in the video description as well")
+
+# step 4
+step_4 = join_discord("join the discord from link given in step 3 or our Video uploaded on Vimeo mentioned in step 1")
+
+# step 5
+step_5 = full_information("for further information about this check our YT channel mentioned in description of Vimeo Video")
+
+# step 6
+step_6 = contact("contact us in our discord as guided in videos in our channel")
+
+no_recoil = step_1 + step_2 + step_3 + step_4 + step_5 + step_6
+enjoy(no_recoil)
+
